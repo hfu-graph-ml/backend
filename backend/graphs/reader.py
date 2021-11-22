9ccf4a1607c8d705c0f4d6ecb6050901b89d193b
Techassi <git@techassi.dev>
2021-11-22 16:17:40 +0100

    Start to add internal graph structure

---
+++ backend/graphs/reader.py
@@ -0,0 +1,2 @@
+# NOTE (Techassi): This file will implement the reader, which reads graph data from (CSV) files and translates it into
+# an internal graph structure
---
+++ backend/graphs/writer.py
@@ -0,0 +1,1 @@
+# NOTE (Techassi): This file will implement the writer, which writes an internal graph structure to CSV graph files
